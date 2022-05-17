01826ae2ef635d8f07f41ccc9f5e7c569ccd3eaf
AbuzarKhan-dev <abuzarkhan5468@gmail.com>
2022-05-17 17:31:03 +0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 const Home: NextPage = () => {
 	return (
-		<div className={styles.container}>
+		<div>
 			<h1 style={{marginTop:"0px",color:"#f1f1f1"}}>Sign in</h1>
 			<Link href={'/home'}>{'>>>>'}</Link>
 		</div>
